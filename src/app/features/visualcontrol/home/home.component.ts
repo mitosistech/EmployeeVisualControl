1d00505843589d0d9f75720459388bc9da3e5188
asuresh86 <asuresh.ui@gmail.com>
2020-01-31 15:28:44 +0530

    Google map UI works

--- src/app/features/visualcontrol/home/home.component.ts
+++ src/app/features/visualcontrol/home/home.component.ts
@@ -30,18 +30,10 @@ export class HomeComponent implements OnInit {
   }
 
   loadMap = () => {
-    var map = new window["google"].maps.Map(this.mapElement.nativeElement, {
-      center: { lat: 24.5373, lng: 81.3042 },
-      zoom: 8
-    });
-
-    var marker = new window["google"].maps.Marker({
-      position: { lat: 24.5373, lng: 81.3042 },
-      map: map,
-      title: "India",
-      draggable: true,
-      animation: window["google"].maps.Animation.DROP
-    });
+    var location = [
+      {'name':'AM', 'lat':'-3.117034', 'lon':'-60.025780'},
+      {'name':'AC', 'lat':'-9.225730', 'lon':'-70.601231'}
+    ]
 
     var contentString =
       '<div id="content">' +
@@ -57,8 +49,59 @@ export class HomeComponent implements OnInit {
       content: contentString
     });
 
-    marker.addListener("click", function() {
-      infowindow.open(map, marker);
-    });
+    // marker.addListener("click", function() {
+    //   infowindow.open(map, marker);
+    // });
+
+    var marker, i;
+    var map = new window["google"].maps.Map(this.mapElement.nativeElement, {
+      center: { lat: -3.117034, lng: -60.025780 },
+      zoom: 4,
+      disableDefaultUI: true,
+      
+      styles: [
+        {elementType: 'geometry', stylers: [{color: '#a6c3d7'}]},
+        {elementType: 'labels.text.stroke', stylers: [{color: '#a6c3d7'}]},
+        {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+        
+        {
+          featureType: 'water',
+          elementType: 'geometry',
+          stylers: [{color: '#7ba7c3'}]
+        },
+        {
+          featureType: 'water',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#515c6d'}]
+        },
+        {
+          featureType: 'water',
+          elementType: 'labels.text.stroke',
+          stylers: [{color: '#7ba7c3'}]
+        }
+      ]
+
+     });
+    for(i = 0; i < location.length; i++){
+      console.log(typeof location[i].lat)
+      
+      marker = new window["google"].maps.Marker({
+        position: new google.maps.LatLng(Number(location[i].lat), Number(location[i].lon)),
+        map: map
+      })
+
+      google.maps.event.addListener(
+        marker,
+        'click',
+        (function(marker, i) {
+          return function() {
+            infowindow.setContent("<div><a routerLink='/clients'>"+location[i].name+"</a></div>")
+            infowindow.open(map, marker)
+          }
+        })(marker, i)
+      )
+    }
+
+
   };
 }
